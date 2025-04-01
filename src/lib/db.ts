a36510fe259f94ab05e2ffd57e8444c5d8e5f350
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2025-04-01 15:59:54 -0300

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,31 @@
-import { Client } from 'pg';
+import { Pool } from 'pg';
 
-export const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
+const getPool = () => {
+  return new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { 
+      rejectUnauthorized: false 
+    } : false,
+  });
+};
 
-client.connect();
+let pool: Pool;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function query(text: string, params?: any[]) {
   try {
-    const result = await client.query(text, params);
-    return result;
+    if (!pool) {
+      pool = getPool();
+      console.log('Pool de conexões com o banco de dados criado');
+    }
+    
+    const client = await pool.connect();
+    try {
+      const result = await client.query(text, params);
+      return result;
+    } finally {
+      client.release();
+    }
   } catch (error) {
     console.error('Database query error:', error);
     throw error;
